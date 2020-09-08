27351f7beb008834837bdc175e4f4097f406f1d2
Attila Adam <attahard@gmail.com>
2020-09-08 20:27:04 +0200

    Spring Cloud Hystrix

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsConfigurationController {
   @Autowired
@@ -12,6 +14,16 @@ public class LimitsConfigurationController {
   @GetMapping("/limits")
   public LimitConfiguration retrieveLimitsFromConfigurations() {
     return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+  }
+
+  @GetMapping("/fault-tolerance-example")
+  @HystrixCommand(fallbackMethod = "fallbackRetrieveConfigurations")
+  public LimitConfiguration retrieveConfigurations() {
+    throw new RuntimeException("Not available");
+  }
+
+  public LimitConfiguration fallbackRetrieveConfigurations() {
+    return new LimitConfiguration(999, 9);
   }
 
 }
